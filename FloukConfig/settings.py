b9196637682a83b46fee9bbafb9d8b5f4b92e60e
Amin Attaei <aminattaei2000@gmail.com>
2025-07-26 10:45:57 +0330

    Added database

--- FloukConfig/settings.py
+++ FloukConfig/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social'
 ]
 
 MIDDLEWARE = [
@@ -63,8 +64,12 @@ WSGI_APPLICATION = 'FloukConfig.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "floukdb",
+        'USER':'postgres',
+        'PASSWORD': '123456',
+        'HOST':'localhost',
+        'PORT':5432,
     }
 }
 
